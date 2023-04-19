ae33390ef980b54e5414f5132fa4006ebb00e01a
Eduardo Suárez Ojeda <esuarez@opencanarias.es>
2023-04-19 15:27:56 +0200

    Avanzando

--- core/src/ledger/ledger.rs
+++ core/src/ledger/ledger.rs
@@ -1,12 +1,45 @@
-use crate::{database::DB, governance::GovernanceAPI, DatabaseManager};
+use std::collections::{HashMap, HashSet};
+
+use crate::{
+    database::DB, event_request::EventRequest, governance::GovernanceAPI, signature::Signature,
+    DatabaseManager, Event, identifier::DigestIdentifier,
+};
+
+use super::errors::LedgerError;
 
 pub struct Ledger<D: DatabaseManager> {
     gov_api: GovernanceAPI,
     database: DB<D>,
+    subject_is_gov: HashMap<DigestIdentifier, bool>,
 }
 
 impl<D: DatabaseManager> Ledger<D> {
     pub fn new(gov_api: GovernanceAPI, database: DB<D>) -> Self {
-        Self { gov_api, database }
+        Self {
+            gov_api,
+            database,
+            subject_is_gov: HashMap::new(),
+        }
+    }
+
+    pub fn init(&self) -> Result<(), LedgerError> {
+        todo!()
+    }
+
+    pub fn event_prevalidated(&self, event: Event) -> Result<(), LedgerError> {
+        self.database.add_event(event);
+        todo!()
+    }
+
+    pub fn genesis(&self, event_request: EventRequest) -> Result<(), LedgerError> {
+        todo!()
+    }
+
+    pub fn event_validated(
+        &self,
+        event: Event,
+        signatures: HashSet<Signature>,
+    ) -> Result<(), LedgerError> {
+        todo!()
     }
 }
